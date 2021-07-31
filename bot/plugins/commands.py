77356938636c851482cf4278c79f6555ccb11892
AKHIDAYANA <87932993+AKHIDAYANA@users.noreply.github.com>
2021-07-31 07:25:45 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'join channel ♻️', url="https://t.me/whitedevilFZg610EpRd0wNGZl"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
+        InlineKeyboardButton('Developers', url='https://t.me/whitedevilFZg610EpRd0wNGZl'),
         InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/whitedevilFZg610EpRd0wNGZl')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
